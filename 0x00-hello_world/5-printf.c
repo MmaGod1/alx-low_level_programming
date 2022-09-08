763163a8ecf011302b252a08ae75a19cba5cc46e
MmaGod1 <gmmaduchukwu@gmail.com>
2022-09-08 16:38:36 -0700

    Print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,13 @@
-{ #include <stdio.h>
+#include <stdio.h>
 
-int main(void) { printf("with proper grammar, but the outcome is a piece of art,\n"); return (0); } 
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+ printf("with proper grammar, but the outcome is a piece of 
+ art,\n");
+ return (0);
+} 
