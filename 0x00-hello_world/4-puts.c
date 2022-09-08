e54ac0423b2b5eb7140e250cd2274c90f01e7ac0
MmaGod1 <gmmaduchukwu@gmail.com>
2022-09-08 15:04:06 -0700

    Put c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #!/bin/bash
- #include <stdio.h>
+#include <stdio.h>
 
-int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); } 
+/**
+ * main - printing
+ *
+ * Return: Always 0 (successful)
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+} 
