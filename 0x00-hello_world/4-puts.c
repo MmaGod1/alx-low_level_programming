40f482ba214add198e74018461e3583b7959277d
MmaGod1 <gmmaduchukwu@gmail.com>
2022-09-09 14:20:51 -0700

    remove

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,11 @@
 
 /**
  * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line."
- *
- * Return: Always 0.
+ * puzzle, followed by a new line,"
+ * Return: Always 0
  */
-int main(void)
+int (main)void
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
+	puts("\"Programming is like building a multiligual puzzle");
 	return (0);
 } 
