bd5c7e1d7952d003c7949930677d9d475f3d7c72
MmaGod1 <gmmaduchukwu@gmail.com>
2022-09-13 09:09:18 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,13 @@
 
 /**
  * main - Prints "Programming is like building a multilingual
- * puzzle, followed by a new line,"
- * Return: Always 0
+ *                puzzle, followed by a new line."
+ *
+ * Return: Always 0.
  */
-int (main)void
+int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
