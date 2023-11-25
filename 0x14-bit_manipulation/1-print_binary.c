5322d622704d844b156f172e3d54e60aa0221842
MmaGod1 <gmmaduchukwu@gmail.com>
2023-11-24 16:38:37 -0800

    binary to decimal

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,7 @@
 /**
  * print_binary - prints the binary representation of a number.
  *  @n: unsigned long int.
- *  Rwturn: void
+ *  Return: void
  */
 void print_binary(unsigned long int n)
 {
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef MAIN_H
+#define MAIN_H
 
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
 void print_binary(unsigned long int n);
+
 #endif
