67640fadbf7d53579973cf00bed82d497650dcb1
MmaGod1 <gmmaduchukwu@gmail.com>
2023-11-28 11:21:27 -0800

    accounting for some conditions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,9 +10,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, wr_fil, i;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content == NULL)
+	if (!text_content)
+	{
+		len = 0;
+	}
+	else
 	{
 		for (i = 0; text_content[i]; i++)
 			;
@@ -21,8 +25,16 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	wr_fil = write(fd, text_content, i);
 
-	if (fd == -1 || wr_fil == -1)
+	if (fd == -1)
 		return (-1);
+	if (text_content != NULL)
+		wr_fil = write(fd, text_content, len);
+
+	if (wr_fil == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 
 	close(fd);
 
