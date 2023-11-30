bd75509d182b7b5f068897ad14db375f8859d14b
MmaGod1 <gmmaduchukwu@gmail.com>
2023-11-30 03:42:11 -0800

    fixing if file name is null & text content doesn't exist

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,13 +10,13 @@ int create_file(const char *filename, char *text_content)
 {
 	int cr_fil, wr_fil, len;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	cr_fil = open(filename, O_RDWR | O_CREAT |
 			O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len]; len++)
 			;
@@ -25,8 +25,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (wr_fil == -1 || wr_fil != len)
 	{
-			close(cr_fil);
-			return (-1);
+		close(cr_fil);
+		return (-1);
 	}
 
 	close(cr_fil);
