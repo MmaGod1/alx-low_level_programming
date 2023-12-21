038113748057d2e7885e7b1943879a93520eec3a
MmaGod1 <gmmaduchukwu@gmail.com>
2023-12-21 08:02:34 -0800

    grnerate password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generstes random pasdwords.
+ *
+ * Return: 0
+ */
+
+#define PASSWORD_LENGTH 8
+
+int main(void)
+{
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
+	const size_t charset_length = sizeof(charset) - 1;
+	char password[PASSWORD_LENGTH + 1];
+	int i;
+
+	srand(time(NULL));
+	for (i = 0; i < PASSWORD_LENGTH; ++i)
+	{
+		password[i] = charset[rand() % charset_length];
+	}
+
+	password[PASSWORD_LENGTH] = '\0';
+	printf("%s\n", password);
+
+	return (0);
+}
