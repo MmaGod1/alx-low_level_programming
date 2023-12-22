ac9547b9b093a55ed451f821ee8577c310626172
MmaGod1 <gmmaduchukwu@gmail.com>
2023-12-22 07:14:32 -0800

    update the code

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,45 +1,51 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * infinite_add - adds two infinit numbers
- * @n1: first infinite number
- * @n2: second infinite number
- * @r: buffer to hold the result of the sumed numbers
- * @size_r: size of the buffer
- *
- * Return: a pointer to the result
+ * infinite_add - adds two numbers
+ * @n1: first number to be added.
+ * @n2: second number to be added.
+ * @r: buffer where the sum would be stored.
+ * @size_r: buffer size:
+ * Return: the pointer to result.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, num1, num2, k = 0, len_n1 = 0, len_n2 = 0;
-	int carry = 0, sum, remainder;
+	int len1 = 0, len2 = 0, carry = 0;
+	int sum, max_len, start, end, res, num1, num2;
+	char temp;
 
-	while (n1[len_n1] != '\0')
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
+	max_len = (len1 > len2 ? len1 : len2) + 1;
+	if (max_len > size_r)
+		return (0);
+	len1--;
+	len2--;
+	res = 0;
+	while (len1 >= 0 || len2 >= 0 || carry)
 	{
-		len_n1++;
-	}
-	while (n2[len_n2] != '\0')
-	{
-		len_n2++;
-	}
-
-	for (i = 0, j = 0; n1[i] != '\0' || n2[j] != '\0' || carry != 0; i++, j++)
-	{
-		num1 = (n1[i] >= '0' && n1[i] <= '9') ? n1[i] - '0' : 0;
-		num2 = (n2[j] >= '0' && n2[j] <= '9') ? n2[j] - '0' : 0;
-
+		num1 = (len1 >= 0 && n1[len1] >= '0' && n1[len1] <= '9') ?
+			(n1[len1] - '0') : 0;
+		num2 = (len2 >= 0 && n2[len2] >= '0' && n2[len2] <= '9') ?
+			(n2[len2] - '0') : 0;
 		sum = num1 + num2 + carry;
-		remainder = sum % 10;
 		carry = sum / 10;
-		if (k >= size_r - 1)
-		{
-			return (0);
-		}
-		else
-		{
-			r[k] = remainder + '0';
-			k++;
-		}
+		r[res] = (sum % 10) + '0';
+		res++;
+		len1--;
+		len2--;
 	}
-	r[k] = '\0';
+	r[res] = '\0';
+	start = 0;
+	end = res - 1;
+	while (start < end)
+	{
+		temp = r[start];
+		r[start] = r[end];
+		r[end] = temp;
+		start++;
+		end--;
+	}
 	return (r);
 }
