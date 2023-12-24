e17887d2a42b858f2be874e9cd8df428ea3e2e70
MmaGod1 <gmmaduchukwu@gmail.com>
2023-12-23 17:33:04 -0800

    print a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,43 +9,43 @@
  */
 void print_buffer(char *b, int size)
 {
-	int i, j, m;
+	int i, j;
 
 	if (size <= 0)
 		printf("\n");
-	else
+
+	for (i = 0; i < size; i += 10)
 	{
-		for (i = 0; i < size; i += 10)
+		printf("%08x: ", i);
+		for (j = i; j < i + 10; j++)
 		{
-			printf("%.8x:", i);
+			if (j < size)
+				printf("%02x", b[j]);
+			else
+				printf("  ");
 
-			for (j = i; j < i + 10; j++)
-			{
-				if (j % 2 == 0)
-				{
-					printf(" ");
-				}
-				if (j < size)
-				{
-					printf("%.2x", b[j]);
-				}
-				else
-				{
-					printf("  ");
-				}
-			}
-			printf(" ");
-
-			for (m = i; m < i + 10; m++)
-			{
-				if (m >= size)
-					break;
-				if (b[m] < 32 && b[m] > 126)
-					printf(".");
-				else
-					printf("%c", b[m]);
-			}
-			printf("\n");
+			if (j % 2 != 0)
+				printf(" ");
 		}
+		printf(" ");
+		for (j = i; j < i + 10; j++)
+		{
+			if (j < size)
+			{
+				if (b[j] >= 32 && b[j] <= 126)
+				{
+					printf("%c", b[j]);
+				}
+				else
+				{
+					printf(".");
+				}
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
 	}
 }
