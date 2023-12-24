1337ad7b0063091c2573ddff46e1f08d8644a050
MmaGod1 <gmmaduchukwu@gmail.com>
2023-12-24 10:43:20 -0800

    locate character in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,15 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+
+	if (s[i] == c)
 	{
-		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
+		return (&s[i]);
 	}
-	return (0);
+	else
+	{
+		return (NULL);
+	}
 }
