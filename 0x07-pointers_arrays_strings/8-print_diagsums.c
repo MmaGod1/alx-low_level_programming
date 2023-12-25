2346953d28428b4233161629b1f680d6a0004582
MmaGod1 <gmmaduchukwu@gmail.com>
2023-12-25 08:59:46 -0800

    change logic

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,13 +12,13 @@ void print_diagsums(int *a, int size)
 {
 	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < (size * size); i++)
+	for (i = 0; i < size; i++)
 	{
 		sum1 += a[i * size + i];
 		/* Access the primary diagonal elements */
 	}
 
-	for (i = 0; i < (size * size); i++)
+	for (i = 0; i < size; i++)
 	{
 		sum2 += a[i * size + (size - 1 - i)];
 		/*Access the secondary diagonal elements */
