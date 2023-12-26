13eff7772637187313a54331282cbb8f6d91d58a
MmaGod1 <gmmaduchukwu@gmail.com>
2023-12-26 10:58:18 -0800

    palidrome and some comments

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
 int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
