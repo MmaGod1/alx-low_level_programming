fabb072c0b9ca9afd954dcb32b876b6770e33805
MmaGod1 <gmmaduchukwu@gmail.com>
2023-12-29 02:19:50 -0800

    return statement

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
 	if (argc < 2)
 	{
 		printf("Error\n");
+		return (1);
 	}
 
 	num = atoi(argv[1]);
@@ -25,6 +26,7 @@ int main(int argc, char *argv[])
 	if (num < 0)
 	{
 		printf("0\n");
+		return (0);
 	}
 
 	for (i = 0; i < 5; i++)
