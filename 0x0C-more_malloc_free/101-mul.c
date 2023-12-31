02e580b02131a2a471ff261346f3c289632a04f4
MmaGod1 <gmmaduchukwu@gmail.com>
2023-12-31 11:00:00 -0800

    multiply numberd

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,7 +11,7 @@ int check_input(char *str)
 {
 	while (*str != '\0')
 	{
-		if (*str < '0' || *str > '9)
+		if (*str < '0' || *str > '9')
 			return (0);
 		str++;
 	}
@@ -19,70 +19,88 @@ int check_input(char *str)
 }
 
 /**
- * multipy - multiply two positive numbers.
+ * multiply - multiply two positive numbers.
  * @num1: first number to be multiplied
  * @num2: second number to multiply.
  *
+ * Return: the product of the numbers.
+ */
+int *multiply(char *num1, char *num2)
+{
+	int len1 = 0, len2 = 0, i, j, mul, sum;
+	int *result;
+
+	while (num1[len1] != '\0')
+		len1++;
+
+	while (num2[len2] != '\0')
+		len2++;
+
+	result = malloc(sizeof(int) * (len1 + len2));
+	if (result == NULL)
+	{
+		printf("Memory allocation failed\n");
+		exit(98);
+	}
+
+	/* Initialize the result array to 0 */
+	for (i = 0; i < len1 + len2; i++)
+	{
+		result[i] = 0;
+	}
+
+	/* Multiply each digit and store the result in the array */
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			mul = (num1[i] - '0') * (num2[j] - '0');
+			sum = result[i + j + 1] + mul;
+			result[i + j] += sum / 10;
+			result[i + j + 1] = sum % 10;
+		}
+	}
+	return (result);
+}
+
+/**
+ * result_print - prints the result of multiplication.
+ * @result: the product of the multiplication
+ * @len: lenght of the result.
+ *
  * Return: nothing.
  */
-int *performMultiplication(char *num1, char *num2) {
-    int len1 = 0, len2 = 0, i, j;
-    while (num1[len1] != '\0') {
-        len1++;
-    }
-    while (num2[len2] != '\0') {
-        len2++;
-    }
+void result_print(int *result, int len)
+{
+	int i = 0;
+	/* Skip leading zeroes */
+	while (i < len && result[i] == 0)
+		i++;
 
-    int *result = (int *)malloc(sizeof(int) * (len1 + len2));
-    if (result == NULL) {
-        printf("Memory allocation failed\n");
-        exit(98);
-    }
-
-    // Initialize the result array to 0
-    for (i = 0; i < len1 + len2; i++) {
-        result[i] = 0;
-    }
-
-    // Multiply each digit and store the result in the array
-    for (i = len1 - 1; i >= 0; i--) {
-        for (j = len2 - 1; j >= 0; j--) {
-            int mul = (num1[i] - '0') * (num2[j] - '0');
-            int sum = result[i + j + 1] + mul;
-            result[i + j] += sum / 10;
-            result[i + j + 1] = sum % 10;
-        }
-    }
-
-    return result;
+	/* Print the result */
+	if (i == len)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		while (i < len)
+		{
+			printf("%d", result[i]);
+			i++;
+		}
+		printf("\n");
+	}
+	free(result);
 }
 
-
-// Function to print the result of multiplication
-void printResult(int *result, int len) {
-    int i = 0;
-    // Skip leading zeroes
-    while (i < len && result[i] == 0) {
-        i++;
-    }
-
-    // Print the result
-    if (i == len) {
-        printf("0\n");
-    } else {
-        while (i < len) {
-            printf("%d", result[i]);
-            i++;
-        }
-        printf("\n");
-    }
-
-    free(result);
-}
-
-
-
+/**
+ * main - the calling function.
+ * @argc: counts arguments passed.
+ * @argv: the numbers passed.
+ *
+ * Return: 0 on success.
+ */
 
 int main(int argc, char *argv[])
 {
@@ -92,7 +110,7 @@ int main(int argc, char *argv[])
 		return (98);
 	}
 
-	if (!isValidInput(argv[1]) || !isValidInput(argv[2]))
+	if (!check_input(argv[1]) || !check_input(argv[2]))
 	{
 		printf("Error\n");
 		return (98);
