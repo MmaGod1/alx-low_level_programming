c23e712369aa6217c41eb966a8433b4e89c14d87
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-06 02:27:49 -0800

    review

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: void.
+ */
+
 void print_all(const char * const format, ...) {
     va_list args;
     va_start(args, format);
