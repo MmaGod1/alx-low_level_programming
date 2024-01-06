98d62855e76fc9a97f79623723589a94049d2c20
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-01-06 13:50:19 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,15 +12,12 @@
 void print_all(const char * const format, ...)
 {
 	char c;
-	int i = 0;
+	int i = 0, printed_values = 0, count = 0;
 	float f = 0.0;
 	char *s = NULL;
-	int printed_values = 0;
-	int count = 0;
 	va_list args;
 
 	va_start(args, format);
-
 	while ((c = format[count]) != '\0' && count < 9)
 	{
 		if (printed_values > 0 && (c == 'c' || c == 'i' || c == 'f' || c == 's'))
@@ -30,24 +27,28 @@ void print_all(const char * const format, ...)
 			case 'c':
 				i = va_arg(args, int);
 				printf("%c", i);
+				printed_values++;
 				break;
 			case 'i':
 				i = va_arg(args, int);
 				printf("%d", i);
+				printed_values++;
 				break;
 			case 'f':
-				f = (float) va_arg(args, double);
+				f = (float)va_arg(args, double);
 				printf("%f", f);
+				printed_values++;
 				break;
 			case 's':
 				s = va_arg(args, char*);
 				if (s == NULL)
 					printf("(nil)");
 				printf("%s", s);
+				printed_values++;
+				break;
 		}
-		break;
+		count++;
 	}
-	count++;
 	va_end(args);
 	printf("\n");
 }
