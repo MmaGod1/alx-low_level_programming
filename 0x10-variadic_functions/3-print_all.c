55205c7acf0839a868d4790a329d9cd7d43ae77c
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-06 02:45:54 -0800

    review

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,43 +8,48 @@
  *
  * Return: void.
  */
-void print_all(const char * const format, ...) {
-    va_list args;
-    va_start(args, format);
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char c;
+	int int_val;
+	float float_val;
+	char *str, format_char;
+	va_list args;
 
-    int i = 0;
-    char c;
-    int int_val;
-    float float_val;
-    char *str;
-
-    while (format[i] != '\0') {
-        char format_char = format[i];
-        
-        if (format_char == 'c') {
-            c = va_arg(args, int);
-            printf("%c", c);
-        }
-        else if (format_char == 'i') {
-            int_val = va_arg(args, int);
-            printf("%d", int_val);
-        }
-        else if (format_char == 'f') {
-            float_val = (float)va_arg(args, double);
-            printf("%f", float_val);
-        }
-        else if (format_char == 's') {
-            str = va_arg(args, char *);
-            if (str == NULL) {
-                printf("(nil)");
-            } else {
-                printf("%s", str);
-            }
-        }
-
-        i++;
-    }
-
-    va_end(args);
-    printf("\n");
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		format_char = format[i];
+		if (format_char == 'c')
+		{
+			c = va_arg(args, int);
+			printf("%c", c);
+		}
+		else if (format_char == 'i')
+		{
+			int_val = va_arg(args, int);
+			printf("%d", int_val);
+		}
+		else if (format_char == 'f')
+		{
+			float_val = (float)va_arg(args, double);
+			printf("%f", float_val);
+		}
+		else if (format_char == 's')
+		{
+			str = va_arg(args, char *);
+			if (str == NULL)
+			{
+				printf("(nil)");
+			}
+			else
+			{
+				printf("%s", str);
+			}
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }
