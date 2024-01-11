e77a4ef3178dad615faddc31470506d0524757ce
MmaGod1 <gmmaduchukwu@gmail.com>
2024-01-11 09:56:05 -0800

    double linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "list.h"
+/**
+ * dlistint_len - counts tge number of elements in a linked dlistint_t list.
+ * @h: points to the head node.
+ * Return: number of nodes.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+ #include "list.h"
+/**
+ * add_dnodeint - adds node at the begining of a list.
+ * @head: pointer to the head of node.
+ * @n: int data
+ * Return: pointer to the head of node in the list.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+
+	return (*head);
+}
