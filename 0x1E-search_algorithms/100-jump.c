04cce8aaf926bcca552a8e6825e7066383efe0d9
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2024-05-12 12:58:09 +0100

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,8 +10,10 @@
  * Return: index of the number
  */
 
-int jump_search(int *array, size_t size, int value) {
-    if (array == NULL) {
+int jump_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+    {
         printf("Array is NULL\n");
         return -1;
     }
@@ -19,25 +21,30 @@ int jump_search(int *array, size_t size, int value) {
     int step = sqrt(size);
     int prev = 0;
     
-    while (array[(step < size ? step : size) - 1] < value) {
+    while (array[(step < size ? step : size) - 1] < value)
+    {
         prev = step;
         step += sqrt(size);
-        if (prev >= size) {
+        if (prev >= size)
+        {
             return -1;
         }
     }
     
     int i = prev;
     printf("Value checked array[%d] = [%d]\n", i, array[i]);
-    while (array[i] < value) {
+    while (array[i] < value)
+    {
         i++;
-        if (i == (step < size ? step : size)) {
+        if (i == (step < size ? step : size))
+        {
             return -1;
         }
         printf("Value checked array[%d] = [%d]\n", i, array[i]);
     }
     
-    if (array[i] == value) {
+    if (array[i] == value)
+    {
         return i;
     }
     
