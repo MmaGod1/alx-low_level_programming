d44935258008621d7d3ba0b205477714e16d4bfd
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2025-08-05 20:10:33 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,16 +6,11 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-		n = -n % 10;
-		_putchar(n + '0');
-		return (n);
-	}
-	else
-	{
-		n = n % 10;
-		_putchar(n + '0');
-		return (n);
-	}
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit = -last_digit;
+    
+	_putchar(last_digit + '0');
+	return last_digit;
 }
