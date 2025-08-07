7f380c118a22781d3d80bda14074fe81b585eefe
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2025-08-07 16:03:03 +0100

    Betty code style

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,50 +6,50 @@
  */
 void print_times_table(int n)
 {
-    int first_num, second_num, product;
+	int first_num, second_num, product;
 
-    if (n < 0 || n > 15)
-    {
-        return;
-    }
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
 
-    for (first_num = 0; first_num <= n; first_num++)
-    {
+	for (first_num = 0; first_num <= n; first_num++)
+	{
 		for (second_num = 0; second_num <= n; second_num++)
 		{
 			product = first_num * second_num;
 
-            if (product > 99)
-            {
-                _putchar((product / 100) + '0');
-                _putchar(((product / 10) % 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-            else if (product > 9)
-            {
-                _putchar(' ');
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-            else if (second_num == 0)
-            {
-                _putchar(product + '0');
-            }
-            else
-            {
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(product + '0');
-            }
+			if (product > 99)
+			{
+				_putchar((product / 100) + '0');
+				_putchar(((product / 10) % 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else if (product > 9)
+			{
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else if (second_num == 0)
+			{
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
 
-            if (second_num == n) {
-                _putchar('\n');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
+			if (second_num == n) {
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
 }
