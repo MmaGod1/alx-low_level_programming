d169f63de5b672efeca9438cdd6fbf7d5ad7762e
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2025-08-19 14:48:00 +0100

    Revert to former code cause it works better.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int d;
 
-	for (d = 0; d < n; d++)
+	for (d = 0; d < n && src[d] != '\0'; d++)
 	{
-		if (src[d] != '\0')
-			dest[d] = src[d];
+		dest[d] = src[d];
 	}
-
 	for (; d < n; d++)
+	{
 		dest[d] = '\0';
-
+	}
 	return (dest);
 }
