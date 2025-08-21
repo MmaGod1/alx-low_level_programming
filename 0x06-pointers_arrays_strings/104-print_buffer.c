dd2f441bf1def3d1681b48151d6c3f7ce17dbe74
MmaGod1 <gmmaduchukwu@gmail.com>
2025-08-21 19:12:23 +0100

    Update code logic

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,44 +7,45 @@
  *  @size: size of buffer
  *  Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int i, j = 0, check = 10;
 
 	if (size <= 0)
 		printf("\n");
 
 	for (i = 0; i < size; i += 10)
 	{
-		printf("%08x: ", i);
-		for (j = i; j < i + 10; j++)
+		printf("%08x:", i);
+
+		check = i + 10;
+
+		while (j < check)
 		{
-			if (j < size)
+			if (j % 2 == 0 && j < size)
+				printf(" %02x", b[j]);
+			else if (j < size)
 				printf("%02x", b[j]);
 			else
-				printf("  ");
-
-			if (j % 2 != 0)
-				printf(" ");
-		}
-		printf(" ");
-		for (j = i; j < i + 10; j++)
-		{
-			if (j < size)
 			{
-				if (b[j] >= 32 && b[j] <= 126)
-				{
-					printf("%c", b[j]);
-				}
+				if (j % 2 == 0)
+					printf("   ");
 				else
-				{
-					printf(".");
-				}
+					printf("  ");
 			}
-			else
-			{
-				printf(" ");
-			}
+			j++;
+		}
+
+		j = i;
+		printf(" ");
+		while (j < check && j < size)
+		{
+			if (b[j] >= 32 && b[j] <= 126)
+				printf("%c", b[j]);
+			else if (b[j] <= 32 || b[j] >= 126)
+				printf(".");
+			j++;
 		}
 		printf("\n");
 	}
