0ba833de509bc8a7b4aee8333f17d0e4172deb4e
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2025-08-22 15:47:27 +0100

    Add parentheses round return statement  - Betty style

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 	int i, j;
 
 	if (*needle == '\0')
-		return haystack;
+		return (haystack);
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
@@ -24,8 +24,8 @@ char *_strstr(char *haystack, char *needle)
 				break;
 		}
 		if (needle[j] == '\0')
-			return &haystack[i];
+			return (&haystack[i]);
 	}
 
-	return NULL;
+	return (NULL);
 }
