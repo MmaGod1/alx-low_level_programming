d6cd1b3e33251801f4955590231e4bf128cb1855
MmaGod1 <gmmaduchukwu@gmail.com>
2025-08-24 14:29:10 +0100

    Simplify logic

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * wildcmp - compares two strings one of which can have
  * the "*" character
@@ -9,20 +10,54 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-	{
-		return (1);
-	}
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
+    /* Both strings ended */
+    if (*s1 == '\0' && *s2 == '\0')
+        return 1;
 
-	if (*s2 == '*')
-	{
-		if (*s1 == '\0')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	}
-	return (0);
+    if (*s1 == *s2)
+        return wildcmp(s1 + 1, s2 + 1);
+
+    if (*s2 == '*')
+    {
+        if (wildcmp(s1, s2 + 1))
+            return 1;
+
+        if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+            return 1;
+
+        return 0;
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+    int r;
+
+    r = wildcmp("main.c", "*.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*a*i*n*.*c*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "main.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "ma********************************c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "***");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m.*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "**.*c");
+    printf("%d\n", r);
+    r = wildcmp("main-main.c", "ma*in.c");
+    printf("%d\n", r);
+    r = wildcmp("main", "main*d");
+    printf("%d\n", r);
+    r = wildcmp("abc", "*b");
+    printf("%d\n", r);
+    return (0);
 }
