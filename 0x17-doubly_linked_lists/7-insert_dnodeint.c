4b0d8ee436c5be48173cbd665eeb8aaecb883f84
root <gmmaduchukwu@gmail.com>
2025-09-06 12:14:14 -0700

    Reuse previous functions

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,6 +6,7 @@
  * @n: data for the new node
  * Return: the address of the new node, or NULL if it failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *current;
@@ -14,36 +15,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL)
 		return (NULL);
 
+	/* If inserting at the beginning, reuse add_dnodeint */
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	current = *h;
+	for (i = 0; i < idx - 1 && current != NULL; i++)
+		current = current->next;
+
+	if (current == NULL)
+		return (NULL);
+
+	/* If inserting at the end, reuse add_dnodeint_end */
+	if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
-
-	if (idx == 0)
-	{
-		new_node->next = *h;
-		new_node->prev = NULL;
-		if (*h != NULL)
-			(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	}
-	current = *h;
-
-	for (i = 0; i < idx - 1 && current != NULL; i++)
-		current = current->next;
-
-	if (current == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
 	new_node->next = current->next;
 	new_node->prev = current;
-	if (current->next != NULL)
-		current->next->prev = new_node;
+	current->next->prev = new_node;
 	current->next = new_node;
 
 	return (new_node);
