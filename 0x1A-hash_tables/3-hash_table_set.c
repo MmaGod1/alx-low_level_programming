354952bb216ff6d4c6963aa0403ac92527f343ba
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2025-09-07 13:41:43 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,17 +17,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
-	/* Get the index for the key */
 	index = key_index((unsigned char *)key, ht->size);
 	temp = ht->array[index];
 	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
-			free(temp->value);
-			temp->value = strdup(value);
-			if (temp->value == NULL)
-				return (0);
+			if (strcmp(temp->value, value) != 0)
+			{
+				free(temp->value);
+				temp->value = strdup(value);
+				if (temp->value == NULL)
+					return (0);
+			}
 			return (1);
 		}
 		temp = temp->next;
