adad4b7e03b6d83bf0f9e9ddff119bdbde2485fa
root <gmmaduchukwu@gmail.com>
2025-09-20 07:05:32 -0700

    Use same data type

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,43 +11,43 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    int left = 0, right = size - 1, mid;
-    size_t i;
+	size_t left = 0, right = size - 1, mid;
+	size_t i;
 
-    if (array == NULL)
-    {
-        printf("Array is NULL\n");
-        return -1;
-    }
+	if (array == NULL)
+	{
+		printf("Array is NULL\n");
+		return -1;
+	}
 
-    while (left <= right)
-    {
-        mid = left + (right - left) / 2;
-        
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            printf("%d", array[i]);
-            if (i < right)
-            {
-                printf(", ");
-            }
-        }
-        printf("\n");
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
 
-        if (array[mid] == value)
-        {
-            return mid;
-        }
-        else if (array[mid] < value)
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
-    }
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
 
-    return -1;
+		if (array[mid] == value)
+		{
+			return mid;
+		}
+		else if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			right = mid - 1;
+		}
+	}
+
+	return -1;
 }
