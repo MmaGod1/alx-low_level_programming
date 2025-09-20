9571c9b2597471a50f63f432cd4a33640e60307a
root <gmmaduchukwu@gmail.com>
2025-09-20 07:22:02 -0700

    Betty compliance

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,32 +11,32 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t step, prev = 0, curr = 0, upper, i;
+	size_t step, prev = 0, curr = 0, upper, i;
 
-    if (!array || size == 0)
-        return (-1);
+	if (!array || size == 0)
+		return (-1);
 
-    step = (size_t)sqrt((double)size);
+	step = (size_t)sqrt((double)size);
 
-    /* Jump in blocks */
-    while (curr < size && array[curr] < value)
-    {
-        printf("Value checked array[%lu] = [%d]\n", curr, array[curr]);
-        prev = curr;
-        curr += step;
-    }
+	/* Jump in blocks */
+	while (curr < size && array[curr] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", curr, array[curr]);
+		prev = curr;
+		curr += step;
+	}
 
-    /* Calculate the upper bound for printing */
-    upper = curr < size ? curr : size;
-    printf("Value found between indexes [%lu] and [%lu]\n", prev, upper);
+	/* Calculate the upper bound for printing */
+	upper = curr < size ? curr : size;
+	printf("Value found between indexes [%lu] and [%lu]\n", prev, upper);
 
-    /* Linear search within the block (safe with min(curr, size-1)) */
-    for (i = prev; i < upper; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return ((int)i);
-    }
+	/* Linear search within the block (safe with min(curr, size-1)) */
+	for (i = prev; i < upper; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return ((int)i);
+	}
 
-    return (-1);
+	return (-1);
 }
